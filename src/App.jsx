1688b9bfc057b7143f69261e979523bb5f872a60
Anderson.Caproni <andersoncaproni0@gmail.com>
2026-03-25 20:02:45 -0300

    material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { Button, Container, Typography } from "@mui/material";
+
 function App() {
-  return ( 
-    <>
-    oi
-    </>
-  )
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Hello Material UI
+      </Typography>
+
+      <Button variant="contained" color="primary">
+        Botão Primário
+      </Button>
+
+      <Button variant="outlined" color="secondary" style={{ marginLeft: 10 }}>
+        Botão Secundário
+      </Button>
+    </Container>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { theme } from "./themes/theme";
 
-createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline /> 
+    <App />
+  </ThemeProvider>
+);
